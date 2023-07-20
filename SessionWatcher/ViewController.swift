6a04a774df32860b3dd924ee19d3c94871da94bd
Vijay Vikram Singh <svijayvikram00@gmail.com>
2023-07-21 00:32:58 +0530

    some changes

--- SessionWatcher/ViewController.swift
+++ SessionWatcher/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextTapped(_ sender: UIButton) {
+        let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondVC") as? SecondVC
+        self.navigationController?.pushViewController(vc!, animated: true)
+    }
+    
 }
 
